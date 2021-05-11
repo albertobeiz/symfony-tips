ec2644f9a7b832aabd2f8974b3fe312f36d99e58
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2021-05-11 15:24:34 +0200

    removed responses

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -9,19 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FeedController
 {
-    #[Route('/', name: 'my_feed', methods: ['GET'])]
+    #[Route('/feed/', name: 'my_feed', methods: ['GET'])]
     public function list(): JsonResponse
     {
-        return new JsonResponse([
-            1,2,3,4,5
-        ]);
+        return new JsonResponse([]);
     }
 
-    #[Route('/{username}', name: 'user_feed', methods: ['GET'])]
+    #[Route('/feed/{username}', name: 'user_feed', methods: ['GET'])]
     public function create(): JsonResponse
     {
-        return new JsonResponse([
-            1,2,3,4,5
-        ]);
+        return new JsonResponse([]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,40 +12,30 @@ class UserController
     #[Route('/users', name: 'users_list', methods: ['GET'])]
     public function list(): JsonResponse
     {
-        return new JsonResponse([
-            1, 2, 3, 4, 5
-        ]);
+        return new JsonResponse([]);
     }
 
     #[Route('/users', name: 'create_user', methods: ['POST'])]
     public function create(): JsonResponse
     {
-        return new JsonResponse([
-            1, 2, 3, 4, 5
-        ]);
+        return new JsonResponse([]);
     }
 
     #[Route('/follow', name: 'follow_user', methods: ['POST'])]
     public function follow(): JsonResponse
     {
-        return new JsonResponse([
-            1, 2, 3, 4, 5
-        ]);
+        return new JsonResponse([]);
     }
 
     #[Route('/dm', name: 'user_dms', methods: ['GET'])]
     public function listDms(): JsonResponse
     {
-        return new JsonResponse([
-            1, 2, 3, 4, 5
-        ]);
+        return new JsonResponse([]);
     }
 
     #[Route('/dm', name: 'send_dm', methods: ['POST'])]
     public function dm(): JsonResponse
     {
-        return new JsonResponse([
-            1, 2, 3, 4, 5
-        ]);
+        return new JsonResponse([]);
     }
 }
