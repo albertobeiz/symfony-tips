952857319da4c23fb0344d59f0bc75c562f46819
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2021-05-15 16:22:01 +0200

    updated Response object

--- src/Controller/User/Post_SignUpUser.php
+++ src/Controller/User/Post_SignUpUser.php
@@ -9,8 +9,6 @@ use App\Repository\UserRepository;
 use App\Services\AnalyticsService;
 use App\Services\EmailService;
 use Doctrine\ORM\EntityManagerInterface;
-use JMS\Serializer\SerializerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,14 +25,14 @@ class Post_SignUpUser
     }
 
     #[Route('/users', name: 'create_user', methods: ['POST'])]
-    public function __invoke(Request $request): User | JsonResponse
+    public function __invoke(Request $request): User | Response
     {
         if($this->userRepository->findOneBy(['email' => $request->get('email')])) {
-            return new JsonResponse('[Error] Email Already Exists', Response::HTTP_CONFLICT);
+            return new Response('[Error] Email Already Exists', Response::HTTP_CONFLICT);
         }
 
         if(strlen($request->get('username')) < 2) {
-            return new JsonResponse('[Error] Username is too short', Response::HTTP_BAD_REQUEST);
+            return new Response('[Error] Username is too short', Response::HTTP_BAD_REQUEST);
         }
 
         $user = new User();
